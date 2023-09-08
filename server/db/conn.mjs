fc2f032c46fc95f674bb595d50a25d934e9f7e7b
Sarina Till <ctill@varsitycollege.co.za>
2023-09-08 09:01:06 +0200

    assing https conf

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,16 +1,24 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = process.env.ATLAS_URI || "";
+const connectionString = "mongodb+srv://sarinatill18:sarinacm@cluster0.mip3zez.mongodb.net/" ;
+//console.log(connectionString);
+//const connectionString = process.env.ATLAS_URI;
+//console.log(connectionString);
 
 const client = new MongoClient(connectionString);
 
 let conn;
 try {
   conn = await client.connect();
+  console.log("successfully connected to Db")
 } catch(e) {
   console.error(e);
 }
 
-let db = conn.db("sample_training");
 
-export default db;
+
+  let db = conn.db("apds");
+
+
+
+  export default db;
--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,20 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-//import records from "./routes/record.mjs";
+import https from "https";
+import path from "path";
+import fs from "fs";
+
+
+
+
+const options = {
+  key: fs.readFileSync("keys/private-key.pem"),                  //Change Private Key Path here
+  cert: fs.readFileSync("keys/certificate.pem"),            //Change Main Certificate Path here
+  }
+
+
+import records from "./routes/record.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -9,9 +22,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//app.use("/record", records);
 
-// start the Express server
-app.listen(PORT, () => {
+app.use("/record", records);
+
+let server = https.createServer(options,app)
+
+// app.get('/',(req,res)=>{
+//   res.send('HTTPS in ExpressJS YASSSSSS')
+// })
+
+//start the Express server
+server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
